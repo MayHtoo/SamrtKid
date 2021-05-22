22ef2d2ae7a2cac9d903ed913e71119bfea8142c
Psyche <mayhtoo18@gmail.com>
2021-05-22 14:55:47 +0630

    modified

--- games/genius-game/js/html5.js
+++
@@ -1,7 +0,0 @@
-( function(document) {
-    var elements = ["header","nav","figure","footer","aside","article","section"];
-
-    for(i=0; i<elements.length; i++) {
-        document.createElement(elements[i]);
-    }
-})(document);
